a0253d21c27a62966a9572e801da54e4721f5158
haloless <sunxiaosong231@gmail.com>
2019-12-24 07:51:59 +0900

    add solve flag and add test

--- cachematrix.R
+++ cachematrix.R
@@ -32,8 +32,40 @@ cacheSolve <- function(x, ...) {
   } else {
     message("solve and cache data")
     m <- x$get()
-    xinv <- solve(m)
+    xinv <- solve(m, ...)
     x$setinv(xinv)
     xinv
   }
 }
+
+
+testCacheMatrix <- function() {
+  nn <- 20 # matrix size
+  
+  # random matrix are in general invertible by theory
+  mraw <- matrix(runif(nn*nn), nrow = nn, ncol = nn)
+  
+  x <- makeCacheMatrix()
+  
+  # now x has nothing inside
+  if (!is.null(x$getinv())) stop("x should cached nothing")
+  
+  # set matrix
+  x$set(mraw)
+  if (!all(x$get() == mraw)) stop("x should set target matrix")
+  if (!is.null(x$getinv())) stop("x should cached nothing")
+  
+  # calculate inverse
+  minv <- cacheSolve(x)
+  
+  # check it is really the inverse
+  merr <- norm(mraw %*% minv - diag(nn))
+  if (merr > 1e-8) stop("matrix inverse check failed")
+  else message(paste("inverse matrix * original matrix err=", merr))
+  
+  # get cached matrix
+  minvcached <- cacheSolve(x)
+  if (!all(minv == minvcached)) stop("cached matrix is not as expected")
+}
+
+
